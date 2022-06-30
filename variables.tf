ae3eaf0a0e2362ceede42bbfa18096a17fb4db42
Simon Lynch <srlynch@gmail.com>
2022-07-01 09:07:25 +1000

    done

--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
-variable vra_url {
+variable "vra_url" {
   type = string
 }
 
-variable vra_refresh_token {
+variable "vra_refresh_token" {
   type = string
 }
 
-variable vra_blueprintId {
+variable "vra_blueprintId" {
   type = string
 }
 
-variable vra_projectId {
+variable "vra_projectId" {
   type = string
 }
